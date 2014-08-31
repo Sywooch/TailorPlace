0532277b611342ae7441db8dbb94fd6fcd56563c
Evgeniy Karavskiy <karavzeka@gmail.com>
2014-08-31 13:30:39 +0400

    db.php убран из отслеживания

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tailor_place',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
