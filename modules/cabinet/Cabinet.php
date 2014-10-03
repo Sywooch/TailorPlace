7054592159580fb163298f401932689c182bde37
Evgeniy Karavskiy <karavzeka@gmail.com>
2014-10-03 21:45:38 +0400

    Восстановил рабочее состояние после обновления Yii

--- modules/cabinet/Cabinet.php
+++ modules/cabinet/Cabinet.php
@@ -20,8 +20,12 @@ class Cabinet extends \yii\base\Module
 	    	if (Yii::$app->getUser()->isGuest) {
 	    		throw new ForbiddenHttpException('У вас нет прав просматривать данную страницу.');
 	    	}
+	        if (isset($action->controller->breadcrumbItem)) {
 	        $action->controller->addBreadcrumbsItem(['label' => 'Личный кабинет', 'url' => ['/cabinet']]);
-	        $action->controller->addBreadcrumbsItem($action->controller->breadcrumbItem);
+	        	$action->controller->addBreadcrumbsItem($action->controller->breadcrumbItem);
+	        } else {
+	        	$action->controller->addBreadcrumbsItem(['label' => 'Личный кабинет']);
+	        }
 	        return true;  // or false if needed
 	    } else {
 	        return false;
